596ca93190d330c52e24c752ae92145c5bb12cde
Weihua <vip_wangweihua@163.com>
2020-10-13 23:32:29 +0800

    dynamic component options

--- smurf-skeleton/src/main/java/com/uhasoft/smurf/skeleton/controller/ProjectController.java
+++ smurf-skeleton/src/main/java/com/uhasoft/smurf/skeleton/controller/ProjectController.java
@@ -1,9 +1,11 @@
 package com.uhasoft.smurf.skeleton.controller;
 
+import com.uhasoft.smurf.common.model.Response;
 import com.uhasoft.smurf.common.util.FileUtils;
 import com.uhasoft.smurf.skeleton.generator.ModuleGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.uhasoft.smurf.skeleton.constant.Constant.TARGET_PATH;
@@ -27,6 +32,9 @@ import static com.uhasoft.smurf.skeleton.constant.Constant.TEMPLATE_FOLDER;
 public class ProjectController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+
+    @Value("${smurf.skeleton.option:}")
+    private String options;
 
     @GetMapping("download")
     public void download(@RequestParam Map<String, String> params, HttpServletResponse response) {
@@ -44,6 +52,18 @@ public class ProjectController {
         } catch (IOException ex) {
             logger.error(ex.getMessage(), ex);
         }
+    }
+
+    @GetMapping("option")
+    public Response<Map<String, List<String>>> getOptions(){
+        String[] dependencies = options.split(";");
+        Map<String, List<String>> result = new HashMap<>(dependencies.length);
+        for(String dependency : dependencies){
+            String[] kv = dependency.split(":");
+            String[] selection = kv[1].split(",");
+            result.put(kv[0], Arrays.asList(selection));
+        }
+        return Response.success(result);
     }
 
 }
